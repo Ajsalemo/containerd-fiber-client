5cf0737a5bc0cfdb8656adc2919ac581290e16bf
Anthony Salemo <ajssalemo@gmail.com>
2024-06-15 21:15:11 -0400

    able to pull from authenticated registries

--- controllers/pullImageController.go
+++ controllers/pullImageController.go
@@ -5,15 +5,22 @@ import (
 	"encoding/json"
 
 	containerd "github.com/containerd/containerd/v2/client"
+	"github.com/containerd/containerd/v2/core/remotes/docker"
 
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 )
 
 type ImageDefintion struct {
-	Registry string `json:"registry"`
-	Image    string `json:"image"`
-	Tag      string `json:"tag"`
+	Registry         string `json:"registry"`
+	Image            string `json:"image"`
+	Tag              string `json:"tag"`
+	IsPublic         *bool  `json:"isPublic"`
+	RegistryUsername string `json:"registryUsername"`
+	RegistryPassword string `json:"registryPassword"`
+	// This field will be populated with the name of the image that was pulled - this is NOT on the incoming request body from the client
+	// All the other fields above are
+	PulledImage string `json:"pulledImage"`
 }
 
 func PullImageController(cxt *fiber.Ctx) error {
@@ -41,10 +48,7 @@ func PullImageController(cxt *fiber.Ctx) error {
 
 	// Close the client later on
 	defer client.Close()
-	// Small helper to change docker.io to what containerd is expecting
-	if imageDefinition.Registry == "docker.io" {
-		imageDefinition.Registry = "docker.io/library"
-	}
+
 	// Do some validation checks on inputs
 	if imageDefinition.Registry == "" {
 		return cxt.Status(400).JSON(fiber.Map{"err": "Registry name is empty or was not provided in the request body"})
@@ -52,15 +56,47 @@ func PullImageController(cxt *fiber.Ctx) error {
 		return cxt.Status(400).JSON(fiber.Map{"err": "Image name is empty or was not provided in the request body"})
 	} else if imageDefinition.Tag == "" {
 		return cxt.Status(400).JSON(fiber.Map{"err": "Tag name is empty or was not provided in the request body"})
+	} else if imageDefinition.RegistryUsername == "" {
+		return cxt.Status(400).JSON(fiber.Map{"err": "Registry username is empty or was not provided in the request body"})
+	} else if imageDefinition.RegistryPassword == "" {
+		return cxt.Status(400).JSON(fiber.Map{"err": "Registry password is empty or was not provided in the request body"})
+	} else if imageDefinition.IsPublic == nil {
+		return cxt.Status(400).JSON(fiber.Map{"err": "IsPublic field is empty or was not provided in the request body"})
 	}
 
-	image, err := client.Pull(ctxStdlib, imageDefinition.Registry+"/"+imageDefinition.Image+":"+imageDefinition.Tag, containerd.WithPullUnpack)
-	if err != nil {
-		zap.L().Error(err.Error())
-		return cxt.Status(500).JSON(fiber.Map{"err": err.Error()})
+	// Check if the pull is public or not - dictated by the `IsPublic` field
+	// If it's not public, we need to provide credentials
+	// Note: We're dereferencing the pointer to get the actual value with '*'
+	// IsPublic is set to an uptyped bool in the ImageDefinition struct to check if it's nil on the incoming Request Body
+	// We need to derefernce or else we get a mismatched type error (untyped bool vs bool)
+	if !*imageDefinition.IsPublic {
+		resolver := docker.NewResolver(docker.ResolverOptions{
+			Credentials: func(host string) (string, string, error) {
+				zap.L().Info(imageDefinition.RegistryUsername)
+				zap.L().Info(imageDefinition.RegistryPassword)
+				return imageDefinition.RegistryUsername, imageDefinition.RegistryPassword, nil
+			},
+		})
+
+		image, err := client.Pull(ctxStdlib, imageDefinition.Registry+"/"+imageDefinition.Image+":"+imageDefinition.Tag, containerd.WithPullUnpack, containerd.WithResolver(resolver))
+		if err != nil {
+			zap.L().Error(err.Error())
+			return cxt.Status(500).JSON(fiber.Map{"err": err.Error()})
+		}
+
+		zap.L().Info("Successfully pulled image " + image.Name())
+		imageDefinition.PulledImage = image.Name()
+		// Else, pull without the resolver since it's public
+	} else {
+		image, err := client.Pull(ctxStdlib, imageDefinition.Registry+"/"+imageDefinition.Image+":"+imageDefinition.Tag, containerd.WithPullUnpack)
+		if err != nil {
+			zap.L().Error(err.Error())
+			return cxt.Status(500).JSON(fiber.Map{"err": err.Error()})
+		}
+
+		zap.L().Info("Successfully pulled image " + image.Name())
+		imageDefinition.PulledImage = image.Name()
 	}
 
-	zap.L().Info("Successfully pulled image " + image.Name())
-
-	return cxt.JSON(fiber.Map{"msg": "Successfully pulled image " + image.Name()})
+	return cxt.JSON(fiber.Map{"msg": "Successfully pulled image " + imageDefinition.PulledImage})
 }
